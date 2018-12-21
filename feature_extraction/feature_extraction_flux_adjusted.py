25b5d03e08dd5b2a67758e97abc658da49c0a055
lfcpeng17 <44904006+lfcpeng17@users.noreply.github.com>
2018-12-21 10:55:33 -0500

    Update feature_extraction_flux_adjusted.py

--- feature_extraction/feature_extraction_flux_adjusted.py
+++ feature_extraction/feature_extraction_flux_adjusted.py
@@ -5,7 +5,7 @@ from feature_extraction import aggregation
 ID = 'object_id'
 target = 'target'
 
-def feature_extraction(df_meta, df):
+def feature_extraction2(df_meta, df):
     # Adjust flux to magnitude, which equals to flux * photoz * photoz
     df = df.merge(df_meta[[ID, 'distmod', 'hostgal_photoz']], on=ID, how='left')
     df = df[df['distmod'].notnull()]
@@ -19,12 +19,12 @@ def feature_extraction(df_meta, df):
 if 'name' == 'main':
     train_meta = pd.read_csv('../input/training_set_metadata.csv')
     train = pd.read_csv('../input/training_set.csv')
-    train_passband_agg, train_object_agg = feature_extraction(train_meta, train)
+    train_passband_agg, train_object_agg = feature_extraction2(train_meta, train)
     train_passband_agg.to_csv('train_passband_aggregation_flux_adjusted.csv', index=False)
     train_object_agg.to_csv('train_object_aggregation_flux_adjusted.csv', index=False)
 
     test_meta = pd.read_csv('../input/test_set_metadata.csv')
     test = pd.read_csv('../input/test_set.csv')
-    test_passband_agg, test_object_agg = feature_extraction(test_meta, test)
+    test_passband_agg, test_object_agg = feature_extraction2(test_meta, test)
     test_passband_agg.to_csv('test_passband_aggregation_flux_adjusted.csv', index=False)
     test_object_agg.to_csv('test_object_aggregation_flux_adjusted.csv', index=False)
